ec53f44c1770c6272aee2c807d7c0b1633263e09
Gonellas <camilamgonella@gmail.com>
2023-05-04 12:11:01 -0300

    Pathfinding Enemy
    Código del pathfinding del enemigo, todavia falta pulir

--- Assets/Scripts/Enemies/EnemyScript.cs
+++ Assets/Scripts/Enemies/EnemyScript.cs
@@ -1,29 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyScript : MonoBehaviour
 {
-    [SerializeField] private Transform player, enemigo;
+    public UnityEngine.AI.NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whasIsPlayer;
 
-    [SerializeField] private float velocidad;
 
-    public bool seguimiento;
+    //Patrulla
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
 
-    private Vector3 playerPos;
+    //Ataque
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
 
     private void Update()
     {
-        FollowPlayer();
+        //Chequear si est� a la vista y en el rango de ataque
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whasIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whasIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+
     }
 
-    private void FollowPlayer()
+    private void Patroling()
     {
-        playerPos = new Vector3(player.position.x, enemigo.position.y, player.position.z);
-        if (seguimiento == true)
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
         {
-            enemigo.transform.position = Vector3.MoveTowards(transform.position, playerPos, velocidad * Time.deltaTime);
-            enemigo.transform.LookAt(player);
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint conseguido
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
         }
     }
+
+    private void SearchWalkPoint()
+    {
+        //Calcular punto random en el rango
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Asegurarnos de que el enemigo no se mueva
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //C�digo de ataque aqu�
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
 }
+
+
+
