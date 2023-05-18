bd188bb85a079a18f52f28b8ce4204dc82338186
Gonellas <camilamgonella@gmail.com>
2023-05-18 20:54:07 -0300

    spawn trigger y ghost

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -12,6 +12,8 @@ public class PlayerStats : CharacterStats
     [SerializeField] private GameObject ghost;
     [SerializeField] private GameObject triggerEmpty;
     [SerializeField] private Transform ghostSpawn;
+    [SerializeField] private Transform triggerSpawn;
+    [SerializeField] private GameObject player;
 
     public bool enemySpawned;
 
@@ -36,18 +38,40 @@ public class PlayerStats : CharacterStats
     //TODO: PLEASE MIGRAR TODAS ESTAS COSAS A UN GAME MANAGER >.<"
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.transform.tag == "Trigger")
-        {
-            Instantiate(ghost, ghostSpawn.position, ghostSpawn.rotation);
-            enemySpawned = true;
-            ghostSpawn.position = new Vector3(14, 13, 41);
+        if (collider.transform.tag == "Trigger") { 
+        List<Vector3> ghostPositions = new List<Vector3>();
+        ghostPositions.Add(ghostSpawn.position = new Vector3(11, 12, 35));
+        ghostPositions.Add(ghostSpawn.position = new Vector3(14, 13, 41));
+        ghostPositions.Add(ghostSpawn.position = new Vector3(3, 11, 44));
+        ghostPositions.Add(ghostSpawn.position = new Vector3(-1, 11, 59));
+        ghostPositions.Add(ghostSpawn.position = new Vector3(10, 12, 64));
+
+        List<Vector3> triggerPositions = new List<Vector3>();
+        triggerPositions.Add(triggerSpawn.position = new Vector3(16.5f, 12.5f, 35f));
+        triggerPositions.Add(triggerSpawn.position = new Vector3(11, 12.5f, 41));
+           
+            if (player.GetComponent<Collider>()){
+                
+                Instantiate(ghost, ghostPositions[0], ghostSpawn.rotation);
+                enemySpawned = true;
+                Destroy(collider);
+                Instantiate(triggerEmpty, triggerPositions[1], triggerSpawn.rotation);
+            }
+
+            //Instantiate(ghost, ghostPositions[0], ghostSpawn.rotation);
+            //enemySpawned = true;
+            //Destroy(collider);
 
             //TODO: SWITCH para que cada vez que toca un trigger cambie a la siguiente posicion:
             //ghostSpawn.position = new Vector3(3, 11, 44);
             //ghostSpawn.position = new Vector3(-1, 11, 59);
             //ghostSpawn.position = new Vector3(10, 12, 64);
-            Destroy(collider);
+            //Destroy(collider);
+
         }
+
+
+
 
         if (collider.transform.tag == "GhostHitBox")
         {
