15a4260d898e8e47dfaa9a8c653c18a2e12cdd83
Gonellas <camilamgonella@gmail.com>
2023-06-11 19:07:57 -0300

    menu opciones sin finalizar y escena nueva manicomio

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();                
+            }
+            else
+            {
+                Pause();           
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ExitToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    //public void Reset()
+    //{
+    //    Time.timeScale = 1f;
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    //}
+
+    //public void Close()
+    //{
+    //    Application.Quit();
+    //}
+}
---
+++ Assets/Scripts/Menu/SceneNav.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNav : MonoBehaviour
+{
+    [SerializeField] int index;
+
+    public void ChangeScene(int i)
+    {
+        index = i;
+        SceneManager.LoadScene(i);
+    }
+
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
